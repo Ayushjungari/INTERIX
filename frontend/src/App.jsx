7953279063d04ac2798d5ac3b68e4836629e43dd
Ayushjungari <ayushjungari@gmail.com>
2026-02-15 16:31:43 +0530

    Fix security vulnerabilities (react-router updated)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
-import "./App.css";
+
 import { SignedOut, SignInButton, SignedIn, SignOutButton, UserButton} from '@clerk/clerk-react';
 function App() {
   
 
   return (
     <>
-      <h1>Welcome to the app</h1>
+      <h1 className="text-red-500 bg-orange-400 p-10 text-3xl">Welcome to the app</h1>
+      <button className="btn btn-secondary">Click me</button>
+
         <SignedOut>
           <SignInButton mode = "modal">
-            <button>Sign In</button>
+            <button>Login</button>
           </SignInButton>
         </SignedOut>
 
